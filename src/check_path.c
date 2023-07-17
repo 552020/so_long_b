7e91103c9c73658c733cd726130a0a42e791d5c3
552020 <stefanolombardo@posteo.de>
2023-07-17 13:19:15 +0200

    refactor flood fill

--- src/check_path.c
+++ src/check_path.c
@@ -1,5 +1,6 @@
 #include "../include/so_long.h"
 
+
 void	check_path(t_game *game)
 {
 	t_game	tmp;
@@ -10,18 +11,17 @@ void	check_path(t_game *game)
 	tmp.collectibles = game->collectibles;
 	tmp.player_x = game->player_x;
 	tmp.player_y = game->player_y;
-	tmp.exit_x = 0;
-	tmp.map_grid = (char **)malloc(tmp.height * sizeof(char *));
-	i = 0;
+	tmp.map_grid = (char **)malloc(sizeof(char *) * tmp.height);
 	if (!tmp.map_grid)
 		exit_with_error("Memory allocation problem", true);
+	i = 0;
 	while (i < tmp.height)
 	{
 		tmp.map_grid[i] = ft_strdup(game->map_grid[i]);
 		i++;
 	}
 	flood_fill(&tmp, tmp.player_y, tmp.player_x);
-	if (!(tmp.exit_x == 1 && tmp.collectibles == 0))
+	if (!(tmp.map_grid[game->exit_y][game->exit_x] == 'F' && tmp.collectibles == 0))
 		exit_with_error("Path error! The exit is not reachable by the player!", false);
 	free_grid(tmp.map_grid, tmp.height);
 }
@@ -30,14 +30,11 @@ int	flood_fill(t_game *tmp, size_t y, size_t x)
 {
 	if (tmp->map_grid[y][x] == '1')
 		return (0);
+	if (tmp->map_grid[y][x] == 'F')
+		return (0);
 	if (tmp->map_grid[y][x] == 'C')
 		tmp->collectibles--;
-	if (tmp->map_grid[y][x] == 'E')
-	{
-		tmp->exit_x = 1;
-		return (0);
-	}
-	tmp->map_grid[y][x] = '1';
+	tmp->map_grid[y][x] = 'F';
 	if (flood_fill(tmp, y + 1, x))
 		return (1);
 	if (flood_fill(tmp, y - 1, x))
