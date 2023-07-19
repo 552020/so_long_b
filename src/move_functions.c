75678492f10318dac04c03040b58c38dbd77944d
552020 <stefanolombardo@posteo.de>
2023-07-19 13:54:19 +0200

    chore: remvoe the move player function

--- src/move_functions.c
+++ src/move_functions.c
@@ -1,44 +1,20 @@
-
-
 #include "../include/so_long.h"
-
-/* 
-mlx_is_key_down: polling approach;
-mlx_key_hook: event-driven approach;
-TODO: actually the mlx_is_key_down should be declared outside of the hook. 
-
-*/
-
-
 
 void	move_hook(mlx_key_data_t keydata, void *tmp)
 {
 	t_game	*game;
 
 	game = (t_game *) tmp;
-	// if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
-		// mlx_close_window(game->mlx);
 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
-		move_player(game, 'w');
-	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
-		move_player(game, 's');
-	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
-		move_player(game, 'd');
-	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-		move_player(game, 'a');
-}
-
-void	move_player(t_game *game, char dir)
-{
-	if (dir == 'w')
 		move_up(game);
-	if (dir == 's')
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
 		move_down(game);
-	if (dir == 'd')
+	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
 		move_right(game);
-	if (dir == 'a')
+	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
 		move_left(game);
 }
+
 
 void	pick_collectibles(t_game *game, int y, int x)
 {
